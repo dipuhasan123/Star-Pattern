93b014494dddf02ba6c4302ba0ba21c71d859348
dipuhasan <depu1994@gmail.com>
2015-08-09 00:15:38 +0600

    Update pattern2.cpp

--- pattern2.cpp
+++ pattern2.cpp
@@ -1,1 +1,28 @@
 
+#include<stdio.h>
+int main()
+{
+    int n, row, col, sp;
+    scanf("%d", &n);
+    for(row = 1; row<=n; row++)
+    {
+
+        for(col = row; col<=n; col++)
+        {
+            printf("*");
+        }
+    for(sp = row-1; sp>=1; sp--)
+    {
+        printf("-");
+    }
+        printf("\n");
+    }
+
+    return 0;
+}
+/*
+*******
+*****--
+***____
+*______
+*/
