9e58e87c2cd020538b4a5855a27ec043caf7b456
dipuhasan <depu1994@gmail.com>
2015-11-19 09:15:25 +0600

    Create Bin2Decimal.cpp

---
+++ Binary_Decimal/Bin2Decimal.cpp
@@ -0,0 +1,31 @@
+//Binary to Decimal
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int b2d(int n)
+{
+    int decimal = 0, i = 0, rem;
+    while(n>0)
+    {
+        rem = n%10;
+        n = n/10;
+        decimal+=rem*pow(2,i);
+        i++;
+    }
+    return decimal;
+}
+
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    cout<<b2d(n);
+
+    return 0;
+}
+
+Input = 1110
+output = 14
