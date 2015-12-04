4b45d29ea84cfa322e7447209e2c3ddc1ae2af67
dipuhasan <depu1994@gmail.com>
2015-12-04 08:59:19 +0600

    Create ispace.cpp

---
+++ c/ispace.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char c;
+   // char str[100];
+    string str;
+
+    getline(cin, str);
+    //int len = str.size();
+    for(int i = 0; i<str[i]; i++){
+        c = str[i];
+        if(isspace(c))
+            c = '\n';
+        putchar(c);
+    }
+    cout<<endl;
+
+    return 0;
+}
+
+
+=========
+Input
+I am dipu
+Output
+I
+am
+dipu
