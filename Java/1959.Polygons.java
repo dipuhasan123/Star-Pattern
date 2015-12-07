9619cd9ca02f7eadce83dd81652ca056998799e8
dipuhasan <depu1994@gmail.com>
2015-12-07 20:46:22 +0600

    Create 1959.Polygons.java

---
+++ Java/1959.Polygons.java
@@ -0,0 +1,21 @@
+import java.math.BigInteger;
+import java.util.*;
+
+public class polygons {
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		BigInteger a, b, t;
+		
+		a = new BigInteger(sc.next());
+		b = new BigInteger(sc.next());
+		
+		System.out.println(a.multiply(b));
+		
+	}
+	
+}
+
+Input:              Output:
+
+1000000 40000       40000000000
